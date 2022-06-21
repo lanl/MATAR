7a33d68939b01c08eeda061d43b7fd375c104271
Erin Heilman <erin.heilman@utexas.edu>
2022-06-21 13:34:14 -0600

    Add files via upload

---
+++ halfspace_cooling.cpp
@@ -0,0 +1,106 @@
+// the idea is to populate a dynamic ragged down array with the 
+// temperatures from the halfspace cooling model as a function of
+// age and depth
+
+// T = Tm erf(z/sqrt(4kt))
+
+// The mantle temperature is 1350, temp will be 1350 at the ridge axis
+// the x direction is controlled by increasing age
+// the y direction is depth
+// k = thermal diffusivity (1 x 10^-6)
+// calculate every 1 million years for 750 Ma years, ignoring that 
+// the max oceanic plate age is 120 Ma
+// have depth increase by 1 km
+
+#include <stdio.h>
+#include <math.h>
+#include <cmath>
+#include <chrono>
+#include <iostream>
+#include <matar.h>
+
+// set up constant parameters
+const int max_age = 1000;
+const double mantle_temp = 1350.0;
+const double thermal_diff = 0.000001;
+
+int main() {
+
+    Kokkos::initialize();
+{
+
+    int depth = 200;
+    auto begin = std::chrono::high_resolution_clock::now(); // start clock
+
+    DynamicRaggedDownArray <double> dyn_ragged_down(max_age, depth); // create array
+
+    // populate array using halfspace cooling
+    // for (int i = 0; i <= max_age; i++) {
+    //     for (int j = 0; j <= depth; j++) {
+    //         if (i == 0 && j == 0) {  // when depth and age are 0, give mantle_temp
+    //             dyn_ragged_down.stride(j)++;
+    //             dyn_ragged_down(i,j) = mantle_temp;
+    //         }
+    //         dyn_ragged_down.stride(j)++;
+    //         dyn_ragged_down(i,j) = mantle_temp * erf(j / (2.0 * sqrt(thermal_diff * (i * 1e6))));
+    //         //depth++;
+    //         // check if we have reached the mantle, if yes, move on to next age
+    //         if (round(dyn_ragged_down(i,j)) == 1350) {
+    //             printf("Depth to mantle %d km, age of lithosphere %d Ma \n", j, i);
+    //             break;
+    //         } 
+    //     } 
+    // }
+
+    // To run this in parallel
+  //  FOR_ALL(i, 0, max_age,
+  //          j, 0, depth, {
+  //              if (i == 0 && j == 0)
+  //              { // when depth and age are 0, give mantle_temp
+  //                  dyn_ragged_down.stride(j)++;
+  //                  dyn_ragged_down(i, j) = mantle_temp;
+  //              }
+  //              double temp = mantle_temp * erf(j / (2.0 * sqrt(thermal_diff * (i * 1e6))));
+  //              dyn_ragged_down.stride(j)++;
+  //              dyn_ragged_down(i, j) = temp;
+
+                // check if we have reached the mantle, if yes, move on to next age
+            //     if (round(dyn_ragged_down(i, j)) == 1350)
+            //     {
+            //         printf("Depth to mantle %d km, age of lithosphere %d Ma \n", j, i);
+            //         break;
+            //     }
+            // });
+
+    DO_ALL(i, 0, max_age, {
+            for (int j = 0; j <= depth; j++) {
+                if (i == 0 && j == 0)
+                { // when depth and age are 0, give mantle_temp
+                    dyn_ragged_down.stride(j)++;
+                    dyn_ragged_down(i, j) = mantle_temp;
+                }
+                double temp = mantle_temp * erf(j / (2.0 * sqrt(thermal_diff * (i * 1e6))));
+                dyn_ragged_down.stride(j)++;
+                dyn_ragged_down(i, j) = temp;
+
+                // check if we have reached the mantle, if yes, move on to next age
+                if (round(dyn_ragged_down(i, j)) == 1350)
+                {
+                    printf("Depth to mantle %d km, age of lithosphere %d Ma \n", j, i);
+                    break;
+                }
+            }
+        });
+
+    // Stop counting time and calculate elapsed time
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin);
+    
+
+    //printf("Depth to mantle at %i Ma, %i \n", max_age, last_depth); 
+    printf("Total time was %f seconds.\n", elapsed.count() * 1e-9);
+}
+    Kokkos::finalize();
+
+    return 0;
+}
