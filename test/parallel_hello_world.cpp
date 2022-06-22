b0e51c4b4a500d9f5d78b2e87c5e02d4fb356839
Erin Heilman <erin.heilman@utexas.edu>
2022-06-22 13:44:37 -0600

    Add files via upload

--- test/parallel_hello_world.cpp
+++ test/parallel_hello_world.cpp
@@ -1,17 +1,14 @@
-#include <omp.h>
 #include <stdio.h>
-#include <stdlib.h>
-
 #include "matar.h"
  
-int main(int argc, char* argv[])
-{
+int main() {
+
     int thread_id;
-    // Beginning of parallel region
+    
     #pragma omp parallel
     {
         thread_id = omp_get_thread_num();
         printf("Hello World... from thread = %d\n", thread_id);
     }
-    // Ending of parallel region
+    
 }
