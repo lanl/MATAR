381176d2c0e43a52bb9e23222eb9e582d772e972
CalvinRoth <rothx195@umn.edu>
2022-07-07 09:28:34 -0600

    website test 4

--- docs/renamer.py
+++ docs/renamer.py
@@ -1,11 +1,28 @@
+import os 
+import sys
 
-test ="index.html" 
+def rename(path):
+    files = os.listdir(path)
+    for file in files:
+        print(file)
+        if(file in [".", "./", "..", "../"]):
+            continue
+        if( os.path.isdir(file)):
+            rename(path+"/"+file)
+        if(".html" not in file):
+            continue
+        test = path + "/" + file 
 
-file = open(test, "r")    
-data = file.read()
-file.close()
+        file = open(test, "r")    
+        data = file.read()
+        file.close()
 
-file = open(test, "w")
-newdata = data.replace("_static", "static")
-file.write(newdata)
-file.close()
+        file = open(test, "w")
+        newdata = data.replace("_static", "static")
+        file.write(newdata)
+        file.close()
+
+
+
+
+rename(".")
