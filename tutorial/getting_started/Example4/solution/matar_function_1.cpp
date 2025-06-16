f9aaf4616b362e807e90c8b649c4fb9cee678199
Jacob Moore <jlmoore@msstate.edu>
2025-06-16 15:08:46 -0500

    ENH: Adding tasks section to matar-fortran interface

--- tutorial/getting_started/Example4/solution/matar_function_1.cpp
+++ tutorial/getting_started/Example4/solution/matar_function_1.cpp
@@ -69,10 +69,16 @@ void square_array_elements_(double* array, int* nx, int* ny)
     int ny_ = *ny;
 
     // create ViewFMatrixDual since array is fortran allocated
+    auto array_2D_dual_view = ViewFMatrixDual<double>(array, nx_, ny_);
 
-    // Note: FOR_ALL/DO_ALL is a macro that expands to a loop over the elements of the array following the 
+    // Note: DO_ALL is a macro that expands to a loop over the elements of the array following the 
     // optimal layout for column major order which is the default for fortran arrays
+    DO_ALL(j, 1, ny_,
+           i, 1, nx_, {
+        array_2D_dual_view(i, j) = pow(array_2D_dual_view(i, j), 2);
+    });
 
+    array_2D_dual_view.update_host();
 }
 
 void sum_array_elements_(double* array, int* nx, int* ny, double* sum_of_elements)
@@ -82,10 +88,15 @@ void sum_array_elements_(double* array, int* nx, int* ny, double* sum_of_element
     int ny_ = *ny;
 
     // create ViewFMatrixDual since array is fortran allocated
+    auto array_2D_dual_view = ViewFMatrixDual<double>(array, nx_, ny_);
 
-    double global_sum = 0.0;
-    double local_sum = 0.0;
-
+    double global_sum;
+    double local_sum;
+    DO_REDUCE_SUM(j, 1, ny_,
+                  i, 1, nx_,
+                  local_sum, {
+        local_sum += array_2D_dual_view(i, j);
+    }, global_sum);
 
     // update sum_of_elements memory location
     *sum_of_elements = global_sum;
